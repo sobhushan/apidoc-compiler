6efa823a5a7f4906f8a37730880a4764afa4af22
Somya Bhushan <100351927+sobhushan@users.noreply.github.com>
2025-03-18 12:58:36 +0530

    Update server.js

--- apidoc-back/server.js
+++ apidoc-back/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
+const path = require("path");
+
 const { exec } = require("child_process");
 
 const app = express();
